c6274ca690d8dd73cc2ece90d46f01de8c9eb0a9
Emre Çil <info.emrecil@gmail.com>
2021-06-09 01:48:00 +0300

    fix.
    Signed-off-by: Kheseyroon <info.emrecil@gmail.com>
    Co-Authored-By: mmkarakaya <karakaya.mehmet.melih@gmail.com>

--- android/src/com/example/interstellarenemies/FirebaseRealtimeUserIntegration.java
+++ android/src/com/example/interstellarenemies/FirebaseRealtimeUserIntegration.java
@@ -10,37 +10,21 @@ import com.google.firebase.database.ValueEventListener;
 
 public class FirebaseRealtimeUserIntegration {
     public static void userAdd() {
-        String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
-        DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference();
-        DatabaseReference uidRef = rootRef.child("users").child(uid);
-        ValueEventListener eventListener = new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                if(!dataSnapshot.exists()) {
-                    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-                    DatabaseReference childRef = FirebaseDatabase.getInstance().getReference().child("users").child(user.getUid());
-                    //childRef.child("friends").child("0").setValue("supportCenterId");
-                    //childRef.child("invites").child("0").setValue("supportCenterId");
-                    childRef.child("money").setValue("500000");
-                    childRef.child("ship_count").setValue("1");
-                    childRef.child("status").setValue("offline");
-                    childRef.child("high_score").setValue("0");
-                    String username = user.getEmail();
-                    if (username == null) username = "";
-                    username = email2userName(username);
-                    childRef.child("name").setValue(username);
-                    childRef.child("rank").setValue("0");
-                    childRef.child("current_ship").setValue("ship");
-                    childRef.child("ships").child("ship").setValue("700004");
-                }
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {}
-        };
-        uidRef.addListenerForSingleValueEvent(eventListener);
-
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        DatabaseReference childRef = FirebaseDatabase.getInstance().getReference().child("users").child(user.getUid());
+        childRef.child("money").setValue("500000");
+        childRef.child("ship_count").setValue("1");
+        childRef.child("status").setValue("offline");
+        childRef.child("high_score").setValue("0");
+        String username = user.getEmail();
+        if (username == null) username = "";
+        username = email2userName(username);
+        childRef.child("name").setValue(username);
+        childRef.child("rank").setValue("0");
+        childRef.child("current_ship").setValue("ship");
+        childRef.child("ships").child("ship").setValue("700004");
     }
+
 
     public static String email2userName(String email) {
         return email.substring(0, email.indexOf("@"));
